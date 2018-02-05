f8f960e5bef52033a829dd6aca8725e5e1261467
lucasluiselli <36174110+lucasluiselli@users.noreply.github.com>
2018-02-05 20:55:18 -0300

    Ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	// Toda linea de instruccion termina con punto y coma (;)
+	//alert("anda");
+	 var nombre;
+	/* alert("nombre"); 
+	alert(nombre);
+	nombre = "Lucas";
+	alert(nombre); */
+	nombre =prompt ("Ingrese su nombre", "natalia natalia");
+	alert("Su nombre es: " + nombre);
+	//var person = prompt("Please enter your name", "Harry Potter");  
 }
 
