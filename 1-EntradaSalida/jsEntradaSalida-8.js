5492037968622deb019276879ce159ac91ff6afa
lucasluiselli <36174110+lucasluiselli@users.noreply.github.com>
2018-02-06 22:07:16 -0300

    ejercicios de entrada y salidaºç
    erererer

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,13 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	numeroUno = document.getElementById('numeroUno'). value;
+	numeroDos = document.getElementById('numeroDos'). value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+	resultado = numero1 % numero2;
+	alert(resultado);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var sueldoConAumento;
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+	sueldoConAumento = sueldo * 0.75;
+	alert(sueldoConAumento);
 
 	
 }
